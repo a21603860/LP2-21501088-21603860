9b09e7d564fca53728fb3fc99fb9c0ed0da3c3b8
clinton <a21603860@alunos.ulht.pt>
2020-09-10 17:38:17 +0100

    nova classe teste

--- src/pt/ulusofona/lp2/fandeisiaGame/TestCreature.java
+++ src/pt/ulusofona/lp2/fandeisiaGame/TestCreature.java
@@ -1,5 +1,22 @@
 package pt.ulusofona.lp2.fandeisiaGame;
 
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class TestCreature {
+
+    @Test
+    public void testeAlteracaoDaorientacao(){
+        Druida game = new Druida(1,"druida","norte",10,2,1);
+
+        game.orientacao= "sul";
+
+        Druida estadoEsperado =  new Druida(1,"druida","sul",10,2,1);
+
+        assertEquals(estadoEsperado, game.orientacao);
+    }
+
+
 
 }
